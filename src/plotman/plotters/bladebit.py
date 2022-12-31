33c40738c9868fce9de3ef9a5829f2885ba0aaaa
Guy Davis <guydavis.ca@gmail.com>
2022-12-31 11:16:24 -0700

    Support for BB's diskplot thread options.

--- src/plotman/plotters/bladebit.py
+++ src/plotman/plotters/bladebit.py
@@ -22,6 +22,9 @@ class Options:
     no_numa: bool = False
     diskplot: bool = False
     diskplot_cache: typing.Optional[str] = None
+    f1threads: typing.Optional[int] = None
+    cthreads: typing.Optional[int] = None
+    p2threads: typing.Optional[int] = None
 
 
 def check_configuration(
@@ -101,6 +104,18 @@ def create_command_line(
     if options.diskplot and options.diskplot_cache:
         args.append("--cache")
         args.append(options.diskplot_cache)
+
+    if options.diskplot and options.f1threads:
+        args.append("--f1-threads")
+        args.append(options.f1threads)
+
+    if options.diskplot and options.cthreads:
+        args.append("--c-threads")
+        args.append(options.cthreads)
+
+    if options.diskplot and options.p2threads:
+        args.append("--p2-threads")
+        args.append(options.p2threads)
 
     if options.diskplot and tmpdir is not None:
         args.append("-t1")
